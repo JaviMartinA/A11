f914385661ab99dadd7f0a38dbd0fd86d47d5e05
Javier Martin Arroyo <javier.martina@campusfp.es>
2023-02-13 14:48:32 +0100

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  num: number = 0;
+  mayorMenor: string = '(Introduce un numero)';
+  numSecret: number = this.numAleatorio(1, 10);
+  constructor(
+  ) {}
 
-  constructor() {}
-
+  numAleatorio(min: number | number, max: number) {
+    return Math.round(Math.random() * (max - min) + parseInt(min.toString(),10));
+  }
+  compruebaNumero() {
+    if (this.num > this.numSecret) {
+      this.mayorMenor = 'menor que ';
+    } else if (this.num < this.numSecret) {
+      this.mayorMenor = 'mayor que ';
+    } else {
+      this.mayorMenor = 'Has acertado! El numero era ';
+    }
+  }
+  reinicia() {
+    this.num = 0;
+    this.numSecret = this.numAleatorio(0, 100);
+    this.mayorMenor = '';
+  }
 }
